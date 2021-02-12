1a69a9a10bab1b1e95b04a90d5d3baa4007fb4cf
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-02-12 13:33:32 -0500

    added data-layer.php with getArrays for sizes, colors, and accessories

---
+++ model/data-layer.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Return a list of colors
+ * @return array
+ */
+function getColors(){
+    $colors = array("red", "green", "blue");
+    return $colors;
+}
+
+/*
+ * Return a list of pet sizes
+ * @return array
+ */
+function getSizes(){
+    $sizes = array("small", "medium", "large");
+    return $sizes;
+}
+
+/*
+ * Return a list of pet accessories
+ * @return array
+ */
+function getAccessories(){
+    $accs = array("bow", "shoes", "rain jacket", "sweater");
+    return $accs;
+}
